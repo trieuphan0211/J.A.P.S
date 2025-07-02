ee60fae892dd6ce21b99cb3da68f8eada6145c84
trieuphan0211 <trieuphan0211@gmail.com>
2025-07-02 17:55:53 +0700

    Add and create schema for Authentication Service

--- microservice/auth-service/src/main/java/vn/stephen/authservice/configuration/SecurityConfiguration.java
+++ microservice/auth-service/src/main/java/vn/stephen/authservice/configuration/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package vn.stephen.eurekaserver.configuration;
+package vn.stephen.authservice.configuration;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,34 +15,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @Log4j2
 public class SecurityConfiguration {
-    @Value("${user.username}")
-    private String userName;
-
-    @Value("${user.password}")
-    private String userPassword;
-
-    @Value("${user.role}")
-    private String userRole;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+                        .anyRequest().permitAll()
+                );
         return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        log.info("Creating user with name: {}, password: {}, role: {}", userName, userPassword, userRole);
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username(userName)
-                .password(userPassword)
-                .roles(userRole)
-                .build();
-        return new InMemoryUserDetailsManager(user);
     }
 }
